6cb628a30efb63022934f00de6a86c05a54bc292
Daniel <danielcaldeira.motta@gmail.com>
2025-12-28 19:28:31 -0300

    refetch table

--- src/features/game-stats/hooks/useFetchGameStats.ts
+++ src/features/game-stats/hooks/useFetchGameStats.ts
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { supabase } from "../../../services/supabase-client.service";
 import type { IGameStatus } from "../../../types/games-stats.types";
 import type { IGamesSupabase } from "../../search-games/types/games.types";
+import { GAME_STATS_QUERY_KEY } from "../../playing/constants";
 
 export const getBacklogGames = async (): Promise<IGamesSupabase[]> => {
   const { data, error } = await supabase
@@ -19,7 +20,7 @@ export const getBacklogGames = async (): Promise<IGamesSupabase[]> => {
 
 export function useFetchGameStats() {
   return useQuery<IGamesSupabase[], Error>({
-    queryKey: ["completed"],
+    queryKey: [GAME_STATS_QUERY_KEY],
     queryFn: getBacklogGames,
   });
 }
