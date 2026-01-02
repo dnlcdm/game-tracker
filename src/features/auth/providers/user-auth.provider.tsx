6197b5274774b1184861833428e3205283ec38c4
Daniel <danielcaldeira.motta@gmail.com>
2026-01-02 18:27:30 -0300

    remove duplicated properties

--- src/features/auth/providers/user-auth.provider.tsx
+++ src/features/auth/providers/user-auth.provider.tsx
@@ -23,8 +23,6 @@ export const UserAuthProvider = ({ children }: PropsWithChildren) => {
         signUpWithEmail,
         signInWithGoogle,
         signOut,
-        signInWithEmail,
-        signUpWithEmail
       }}
     >
       {children}
